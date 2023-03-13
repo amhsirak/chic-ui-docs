3fd478aa8358b5d032e3ce167242403c2bf3e702
karishmas6 <carishmashukla28@gmail.com>
2023-03-13 17:39:48 +0530

    fix: render Drawer when React app has hydrated

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import { Drawer } from 'chic-ui'
+import BrowserOnly from '@docusaurus/BrowserOnly'
 
 
 export function BasicDrawer() {
     const [open, setOpen] = React.useState(false);
     const handleClose = () => setOpen(!open);
     return (
-        <>
-            <button onClick={handleClose}>Open Drawer</button>
-            <Drawer
-                open={open}
-                size="default"
-                closable={true}
-                placement="bottom"
-                header="Header Content"
-                footer="Footer content"
-                onClose={handleClose}
-            >
-                <div>Some contents...</div>
-                <div> Some contents...</div>
-                <div> Some contents...</div>
-            </Drawer>
-        </>
-    );
+        <BrowserOnly>
+            {() => (
+                <>
+                    <button onClick={handleClose}>Open Drawer</button>
+                    <Drawer
+                        open={open}
+                        size="default"
+                        closable={true}
+                        placement="bottom"
+                        header="Header Content"
+                        footer="Footer content"
+                        onClose={handleClose}
+                    >
+                        <div>Some contents...</div>
+                        <div> Some contents...</div>
+                        <div> Some contents...</div>
+                    </Drawer>
+                </>
+            )}
+            );
 }
