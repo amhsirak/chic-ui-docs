4b0923a8fcfcbc3c55d977c955de10c275bf0b0e
karishmas6 <carishmashukla28@gmail.com>
2023-04-03 22:46:55 +0530

    feat: set fill as black and stroke as white

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -38,7 +38,7 @@ export function IconSteps() {
     return (
         <Steps active={1} type="primary">
             <Steps.Step title="Step One" subTitle="Step one details" icon={
-                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="black" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
             } />
             <Steps.Step title="Step Two" subTitle="Step two details" />
             <Steps.Step title="Step Three" subTitle="Step three details"  />
