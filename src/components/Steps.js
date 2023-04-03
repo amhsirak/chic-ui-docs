1a19cbc351be0a00d1ecd27238588c93e6d96d36
karishmas6 <carishmashukla28@gmail.com>
2023-04-03 22:50:19 +0530

    feat: SubtitleSteps fnx

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -46,3 +46,14 @@ export function IconSteps() {
         </Steps>
     );
 }
+
+export function SubtitleSteps() {
+    return (
+        <Steps active={1} type="primary">
+            <Steps.Step title="Step One" subTitle="Step one details"  />
+            <Steps.Step title="Step Two" subTitle="Step two details" />
+            <Steps.Step title="Step Three" subTitle="Step three details"  />
+            <Steps.Step title="Step Four" subTitle="Step four details"  />
+        </Steps>
+    );
+}
